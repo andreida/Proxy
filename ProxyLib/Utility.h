3476790e4b46191cd24ab5adb48cc88f27017e11
andreida <danchi@tut.by>
2015-11-24 17:58:55 +0300

    implementation of RequestParser is in progress...

---
+++ ProxyLib/Utility.h
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace ProxyLib {
+
+template <class T, class Alloc>
+void Append(T val, std::vector<T, Alloc>& v)
+{
+    v.insert(v.end(), val);
+}
+
+template <class Iter, class Alloc>
+void Append(Iter begin, Iter end, std::vector<typename Iter::value_type, Alloc>& v)
+{
+    v.insert(v.end(), begin, end);
+}
+
+template <class T, class Alloc>
+void Append(std::initializer_list<T> ilist, std::vector<T, Alloc>& v)
+{
+    v.insert(v.end(), ilist);
+}
+
+template <class StringT> 
+struct LexicographicalCompare
+{
+    bool operator()(const StringT& l, const StringT& r) const
+    {
+        return std::lexicographical_compare(l.begin(), l.end(), r.begin(), r.end()
+            , [this](StringT::value_type ch1, StringT::value_type ch2)
+              {
+                  return std::tolower(ch1, locale_) < std::tolower(ch2, locale_);
+              });
+    }
+
+private:
+
+    std::locale locale_;
+};
+
+}
